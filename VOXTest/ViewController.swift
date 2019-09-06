761623cc0b46d5f84f1289e7badaed82bf43fd60
Andrew <incriptos@gmail.com>
2019-09-06 11:37:19 +0300

    Initial Commit

---
+++ VOXTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VOXTest
+//
+//  Created by Andrew Vashulenko on 9/6/19.
+//  Copyright © 2019 Andrew Vashulenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
